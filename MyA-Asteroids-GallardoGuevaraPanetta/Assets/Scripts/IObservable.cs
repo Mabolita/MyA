3b9225ac5aa96cf6a9db52fe91f123e59069f6fb
Agus Vila <30276554+agusgoku87@users.noreply.github.com>
2022-07-08 21:49:41 -0300

    *

--- MyA-Asteroids-GallardoGuevaraPanetta/Assets/Scripts/IObservable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IObservable
-{
-    void Subscribe(IObserver observer);
-    void Unsubscribe(IObserver observer);
-}
